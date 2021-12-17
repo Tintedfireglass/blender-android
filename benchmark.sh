1e8c09e09af1b51e898e9005806fdcd5c35410f1
TintedFireglass <tintedfireglass@hotmail.com>
2021-12-17 17:02:00 +0530

    Create benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,6 @@
+#Running the blender BMW benchmark using the cpu
+#Run this after the install.sh script
+dnf install wget zip unzip 
+wget https://download.blender.org/demo/test/BMW27_2.blend.zip
+unzip BMW27_2.blend.zip
+blender -b bmw27/bmw27_cpu.blend -o render.png  -f -2
