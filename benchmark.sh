5b04e9a3111525947f832d57b608dc5f1559adc3
TintedFireglass <tintedfireglass@hotmail.com>
2022-03-02 18:06:11 +0530

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #Running the blender BMW benchmark using the cpu
 #Run this after the install.sh script
-pacman -S wget zip unzip 
+apt install wget zip unzip 
 wget https://download.blender.org/demo/test/BMW27_2.blend.zip
 unzip BMW27_2.blend.zip
 blender -b bmw27/bmw27_cpu.blend -o render.png  -f 1
